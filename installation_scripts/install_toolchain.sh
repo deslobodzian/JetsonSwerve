8b17283d8d7498a5f439d6bd142209ab8039fc6b
deslobodzian <de.slobodzian@gmail.com>
2023-05-28 13:08:28 -0400

    Toolchain installation seems good, phoenix-pro is the only issue right now, maybe just copy the jetson pheonix-pro things into the sysroot if I cannot find a better way.

--- installation_scripts/install_toolchain.sh
+++ installation_scripts/install_toolchain.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "Downloading Toolchain from NVidia Website"
+echo "Downloading "
 mkdir ~/toolchain/
 cd ~/toolchain/
-wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.3-2021.07/binrel/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu.tar.xz
-tar -xf gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu.tar.xz
-cd gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu
+wget https://toolchains.bootlin.com/downloads/releases/toolchains/aarch64/tarballs/aarch64--glibc--stable-2021.11-1.tar.bz2
+tar -xf aarch64--glibc--stable-2021.11-1.tar.bz2
+rm aarch64--glibc--stable-2021.11-1.tar.bz2
+cd aarch64--glibc--stable-2021.11-1/
 
 # Get the absolute path to the toolchain installation directory
 TOOLCHAIN_DIR=$(pwd)
@@ -14,10 +15,89 @@ TOOLCHAIN_DIR=$(pwd)
 echo "export JETSON_TOOLCHAIN=${TOOLCHAIN_DIR}" >> ~/.bashrc
 
 # Create a CMake toolchain file for Jetson
-echo "set(CMAKE_SYSTEM_NAME Linux)" > ${TOOLCHAIN_DIR}/JetsonToolchain.cmake
-echo "set(CMAKE_SYSTEM_PROCESSOR aarch64)" >> ${TOOLCHAIN_DIR}/JetsonToolchain.cmake
-echo "set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-gcc)" >> ${TOOLCHAIN_DIR}/JetsonToolchain.cmake
-echo "set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++)" >> ${TOOLCHAIN_DIR}/JetsonToolchain.cmake
+TOOLCHAIN_FILE="${TOOLCHAIN_DIR}/JetsonToolchain.cmake"
+INSTALL_PREFIX="${TOOLCHAIN_DIR}/aarch64-buildroot-linux-gnu/sysroot"
 
-# Reload the .bashrc file to apply the environment variable
+echo "export JETSON_TOOLCHAIN_FILE=\"${TOOLCHAIN_FILE}\"" >> ~/.bashrc
+echo "export JETSON_INSTALL_PREFIX=\"${INSTALL_PREFIX}\"" >> ~/.bashrc
+
+echo "Creating ${TOOLCHAIN_FILE}..."
+
+cat << EOF > "${TOOLCHAIN_FILE}"
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
+
+set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_DIR}/aarch64-buildroot-linux-gnu/sysroot)
+set(CMAKE_SYSROOT ${TOOLCHAIN_DIR}/aarch64-buildroot-linux-gnu/sysroot)
+
+set(CMAKE_TYPE_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/aarch64-buildroot-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/bin/aarch64-buildroot-linux-gnu-g++)
+
+if (NOT CMAKE_C_COMPILER OR NOT CMAKE_CXX_COMPILER)
+    message(FATAL_ERROR "Can't find C/C++ cross compiler for Jetson")
+endif()
+
+set(CMAKE_AR ${TOOLCHAIN_DIR}/bin/aarch64-buildroot-linux-gnu-ar CACHE FILEPATH "" FORCE)
+set(CMAKE_RANLIB ${TOOLCHAIN_DIR}/bin/aarch64-buildroot-linux-gnu-ranlib)
+set(CMAKE_LINKER ${TOOLCHAIN_DIR}/bin/aarch64-buildroot-linux-gnu-ld)
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT -Wl,--allow-shlib-undefined)
+
+set(CMAKE_CUDA_FLAGS "-ccbin \${CMAKE_CXX_COMPILER} -Xcompiler -fPIC" CACHE STRING "" FORCE)
+
+EOF
+
 source ~/.bashrc
+
+echo "Installing libraries to toolchain..."
+mkdir temp_libraries && cd temp_libraries
+echo "Installing zlib..."
+wget https://github.com/madler/zlib/releases/download/v1.2.13/zlib-1.2.13.tar.gz
+tar -xf zlib-1.2.13.tar.gz
+cd zlib-1.2.13
+mkdir build && cd build
+cmake -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}" -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" ..
+make -j16
+make install
+
+echo "zlib installed, exiting folder"
+cd ../..
+echo "Installing OpenSSL..."
+
+wget https://www.openssl.org/source/openssl-1.1.1l.tar.gz
+tar -xf openssl-1.1.1l.tar.gz
+cd openssl-1.1.1l
+
+./Configure linux-aarch64 \
+    --cross-compile-prefix=${TOOLCHAIN_DIR}/bin/aarch64-buildroot-linux-gnu- \
+    --prefix=${INSTALL_PREFIX}
+
+make -j16
+make install
+
+echo "Installed OpenSSL to toolchain, exiting folder"
+cd ..
+echo "Installing LIBSSH..."
+
+wget https://git.libssh.org/projects/libssh.git/snapshot/libssh-0.9.7.tar.gz
+tar -xf libssh-0.9.7.tar.gz
+cd libssh-0.9.7/
+mkdir build && cd build
+cmake -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}" -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" ..
+make -j16
+make install
+echo "Installed LIBSSH to toolchain, exiting folder"
+cd ../..
+
+echo "Installation complete, cleaning up files..."
+cd ..
+rm -r temp_libraries 
+echo "Installation finished."
+
--- installation_scripts/install_wpilib.sh
+++ installation_scripts/install_wpilib.sh
@@ -1,27 +1,57 @@
 #!/bin/bash
 
-echo "Download WPILib from github release... "
+# Set the destination folder for the built libraries
+LIB_DESTINATION="$(dirname "$(pwd)")/lib"
+
+# Check if destination folder exists and if the .so files are present
+if [ -d "$LIB_DESTINATION" ] && [ -f "$LIB_DESTINATION/libwpilibc.so" ] && [ -f "$LIB_DESTINATION/libntcore.so" ] && [ -f "$LIB_DESTINATION/libwpimath.so" ] && [ -f "$LIB_DESTINATION/libwpiutil.so" ]; then
+    echo "WPILib libraries already exist in $LIB_DESTINATION. Skipping installation."
+    exit 0
+fi
+
+USE_TOOLCHAIN=false
+
+# Check if --use-toolchain flag is present
+if [[ $* == *--use-toolchain* ]]; then
+    USE_TOOLCHAIN=true
+    echo "Using the toolchain for installation."
+fi
 
 wget https://github.com/wpilibsuite/allwpilib/archive/refs/tags/v2023.4.3.tar.gz
 echo "Extracting WPILib Linux"
 
 tar -xzvf v2023.4.3.tar.gz
+rm v2023.4.3.tar.gz
 
 echo "Changing Directory to WPILib"
 cd allwpilib-2023.4.3
 
-echo "Building and configuring CMake"
-
 mkdir build_wpi
 cd build_wpi
 
-cmake .. -DWITH_TOOLCHAIN_FILE=$JETSON_TOOLCHAIN/JetsonToolchain.cmake -DWITH_JAVA=OFF -DWITH_SHARED_LIBS=OFF -DWITH_CSCORE=OFF -DWITH_NTCORE=ON -DWITH_WPIMATH=ON -DWITH_WPILIB=ON -DWITH_EXAMPLES=OFF -DWITH_TESTS=OFF -DWITH_GUI=OFF -DWITH_SIMULATION_MODULES=OFF
+# Configure CMake with the toolchain and necessary options if --use-toolchain flag is present
+if [ "$USE_TOOLCHAIN" = true ]; then
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=$JETSON_TOOLCHAIN_FILE -DCMAKE_INSTALL_PREFIX=$JETSON_INSTALL_PREFIX -DWITH_JAVA=OFF -DWITH_CSCORE=OFF -DWITH_NTCORE=ON -DWITH_WPIMATH=ON -DWITH_WPILIB=ON -DWITH_EXAMPLES=OFF -DWITH_TESTS=OFF -DWITH_GUI=OFF -DWITH_SIMULATION_MODULES=OFF
+else
+    cmake .. -DWITH_JAVA=OFF -DWITH_CSCORE=OFF -DWITH_NTCORE=ON -DWITH_WPIMATH=ON -DWITH_WPILIB=ON -DWITH_EXAMPLES=OFF -DWITH_TESTS=OFF -DWITH_GUI=OFF -DWITH_SIMULATION_MODULES=OFF
+fi
 
-sudo make install -j16
+# Build WPILib
+make -j16
+make install
 
-echo "Installation complete removing temp files"
+# Create the destination folder for the libraries if it doesn't exist
+# mkdir -p $LIB_DESTINATION
 
-cd ..
-cd ..
-sudo rm -r allwpilib-2023.4.3
-rm v2023.4.3.tar.gz
+# Copy the shared object files to the lib folder
+# cp -L ./lib/libwpilibc.so $LIB_DESTINATION
+# cp -L ./lib/libntcore.so $LIB_DESTINATION
+# cp -L ./lib/libwpimath.so $LIB_DESTINATION
+# cp -L ./lib/libwpiutil.so $LIB_DESTINATION
+
+# Cleanup
+cd ../..
+rm -rf allwpilib-2023.4.3
+
+echo "WPILib build completed. Shared object files copied to $LIB_DESTINATION"
+
